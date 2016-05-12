889474589e7ef98a82c68ddbd093619ab3cb2a54
Kris Sparks <kbs5280@gmail.com>
2016-05-12 09:53:22 -0600

    Intial commit

---
+++ bob.rb
@@ -0,0 +1,19 @@
+require 'pry'
+
+class Bob
+
+  def hey(remark)
+    if remark[-1] == "?" && remark != remark.upcase
+      'Sure.'
+    elsif remark.strip == ''
+      'Fine. Be that way!'
+    elsif remark[-1] != "!" && remark.split(",").map(&:to_i).inject(:+) > 0
+      'Whatever.'
+    elsif remark == remark.upcase
+      'Whoa, chill out!'
+    else
+      'Whatever.'
+    end
+  end
+
+end
